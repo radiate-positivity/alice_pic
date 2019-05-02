ac16707fbc64fbdeaabcd8e868b0222904e6aa75
radiate-positivity <45203491+radiate-positivity@users.noreply.github.com>
2019-05-02 18:54:29 +0400

    Update api.py

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ import requests
 
 def add_im(url_im):
     headers = {
-        'Authorization': 'OAuth <OAuth-AQAAAAAgybA_AAT7o5bkMo-x70LHvAzRp8P6GCM>',
+        'Authorization': 'OAuth AQAAAAAgybA_AAT7o5bkMo-x70LHvAzRp8P6GCM',
         'Content-Type': 'application/json'
     }
     data = '{ "url": "' + url_im + '" }'
@@ -18,9 +18,9 @@ def add_im(url_im):
 
 def del_im(id_im):
     headers = {
-        'Authorization': 'OAuth <OAuth-AQAAAAAgybA_AAT7o5bkMo-x70LHvAzRp8P6GCM>'
+        'Authorization': 'OAuth AQAAAAAgybA_AAT7o5bkMo-x70LHvAzRp8P6GCM'
     }
-    requests.delete('https://dialogs.yandex.net/api/v1/skills/%3Cидентификатор%20навыка%3E/images/{}'.format(id_im),
+    requests.delete('https://dialogs.yandex.net/api/v1/skills/74ad82e4-64fc-4474-97e6-3237ce9b2501/images/{}'.format(id_im),
                     headers=headers)
 
 def start():
