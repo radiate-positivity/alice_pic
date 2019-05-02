13ac3a413003675a44f08fb7d2c74cf4120a677c
radiate-positivity <45203491+radiate-positivity@users.noreply.github.com>
2019-05-02 18:29:31 +0400

    Update api.py

--- api.py
+++ api.py
@@ -6,11 +6,10 @@ def add_im(url_im):
         'Authorization': 'OAuth <OAuth-AQAAAAAgybA_AAT7o5bkMo-x70LHvAzRp8P6GCM>',
         'Content-Type': 'application/json'
     }
-
     data = '{ "url": "' + url_im + '" }'
-
     response = requests.post('https://dialogs.yandex.net/api/v1/skills/74ad82e4-64fc-4474-97e6-3237ce9b2501/images',
-                             headers=headers, data=data)
+                             headers=headers, data=data).json()
+    
     return response['image']['id']
 
 
